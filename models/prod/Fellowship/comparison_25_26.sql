aae372ec0ccaee6b6ca2ff647ee3de6d6f22e133
TechBhumi <tech@bhumi.ngo>
2026-02-02 21:50:12 +0530

    Update comparison_25_26.sql

--- models/prod/Fellowship/comparison_25_26.sql
+++ models/prod/Fellowship/comparison_25_26.sql
@@ -8,9 +8,10 @@ with all_student_id as (
         pm_name_base as pm_name,
         fellow_name_base as fellow_name,
         student_grade_base as student_grade,
-        rc_learning_level_status_baseline_base as rc_status,
-        rf_baseline_growth_base as rf_status,
-        math_learning_level_status_baseline_base as math_status
+        rc_learning_level_status_baseline_base as rc_level,
+        rf_baseline_growth_base as rf_growth,
+        rf_level_baseline_base as rf_level,
+        math_learning_level_status_baseline_base as math_level
     from {{ ref('baseline_25_26_stg') }}
     where student_id_base is not null
     
@@ -26,9 +27,10 @@ with all_student_id as (
         pm_name_mid as pm_name,
         fellow_name_mid as fellow_name,
         student_grade_mid as student_grade,
-        rc_learning_level_status_midline_mid as rc_status,
-        rf_midline_growth_mid as rf_status,
-        math_learning_level_status_midline_mid as math_status
+        rc_learning_level_status_midline_mid as rc_level,
+        rf_midline_growth_mid as rf_growth,
+        rf_level_midline_mid as rf_level,
+        math_learning_level_status_midline_mid as math_level
     from {{ ref('midline_25_26_stg') }}
     where student_id_mid is not null
 
@@ -49,14 +51,16 @@ select
     s.pm_name,
     s.fellow_name,
     s.student_grade,
-    s.rc_status,
-    s.rf_status,
-    s.math_status,
+    s.rc_level,
+    s.rf_level,
+    s.math_level,
+    s.rf_growth,
 
     -- Baseline columns
     b.student_id_base is not null as baseline_attendance,
     b.rc_learning_level_status_baseline_base,
     b.rf_baseline_growth_base,
+    b.rf_level_baseline_base,
     b.math_learning_level_status_baseline_base,
     b.cohort_base,
 
@@ -64,6 +68,7 @@ select
     m.student_id_mid is not null as midline_attendance,
     m.rc_learning_level_status_midline_mid,
     m.rf_midline_growth_mid,
+    m.rf_level_midline_mid,
     m.math_learning_level_status_midline_mid,
     m.cohort_mid
     -- Endline columns
