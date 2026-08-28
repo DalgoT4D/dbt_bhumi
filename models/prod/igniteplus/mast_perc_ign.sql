1fe6857f10d6fad42e776fb356c17553d3638194
TechBhumi <130547889+TechBhumi@users.noreply.github.com>
2026-08-28 21:57:30 +0530

    sqlfluff fixing

--- models/prod/igniteplus/mast_perc_ign.sql
+++ models/prod/igniteplus/mast_perc_ign.sql
@@ -6,16 +6,17 @@
 with completion_perc as (
     select 
         *,
-    case
-      when planned_duration_days is null
-        or actual_duration_days is null
-        or actual_duration_days = 0
-        then null
-      else least(
-        (planned_duration_days::numeric / actual_duration_days::numeric) * 100,
-        100
-      )
-    end as completion_perc
+        case
+            when
+                planned_duration_days is null
+                or actual_duration_days is null
+                or actual_duration_days = 0
+                then null
+            else least(
+                (planned_duration_days::numeric / actual_duration_days::numeric) * 100,
+                100
+            )
+        end as completion_perc
     from {{ ref('master_data_ign_25_26') }}
 )
 
