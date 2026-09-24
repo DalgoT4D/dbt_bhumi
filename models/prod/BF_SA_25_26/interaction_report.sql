34fc8ffc48a521470e09e18035a704ec34f50432
TechBhumi <130547889+TechBhumi@users.noreply.github.com>
2026-09-24 19:11:43 +0530

    updated on interaction report and class_upd_int

--- models/prod/BF_SA_25_26/interaction_report.sql
+++ models/prod/BF_SA_25_26/interaction_report.sql
@@ -21,13 +21,18 @@ select
     helo_circles_count,
     ptms_count,
     case
-        when odc_count + checkin_count = 0 then 'Likely unreported'
+        when coalesce(odc_count, 0) + coalesce(checkin_count, 0) = 0
+            then 'Likely unreported'
         else 'Reported'
     end as pm_unreported,
     case
         when
-            school_leader_checkin_count + teacher_circle_count + community_visit_count
-            + teaching_hours_sum + helo_circles_count + ptms_count = 0
+            coalesce(school_leader_checkin_count, 0)
+            + coalesce(teacher_circle_count, 0)
+            + coalesce(community_visit_count, 0)
+            + coalesce(teaching_hours_sum, 0)
+            + coalesce(helo_circles_count, 0)
+            + coalesce(ptms_count, 0) = 0
             then 'Likely unreported'
         else 'Reported'
     end as fellow_unreported
